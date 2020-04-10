b16f322e96e2e9c459e1b11cd18f4691992e64e8
Kalen <ktwmgoo@gmail.com>
2020-04-09 18:00:37 -1000

    Requirement 3: Code

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function greet(name) {
+  // checks for uppercase
+  if (name && name == name.toUpperCase()) {
+    return `HELLO ${name}!`;
+  }
+
+  // otherwise returns the name or the default phrase if there's no name
   return `Hello, ${name || "my friend"}.`;
 }
 
