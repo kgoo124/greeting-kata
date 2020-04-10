e57f78ef873744561dbedd04047eb4a3486c18f0
Kalen <ktwmgoo@gmail.com>
2020-04-09 18:28:37 -1000

    Requirement 5: Code

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function greet(name) {
   // checks for an array of names
-  if (Array.isArray(name)) return `Hello, ${name[0]} and ${name[1]}.`;
+  if (Array.isArray(name)) {
+    // two names
+    if (name.length == 2) {
+      return `Hello, ${name[0]} and ${name[1]}.`;
+    }
+
+    // more than two names
+    return `Hello,${name.map((n, i, arr) => {
+      // if name is the last name in the list, print "and" followed by the name
+      if (i == arr.length - 1) {
+        return ` and ${n}.`;
+      }
+      // else, just print the name
+      return ` ${n}`;
+    })}`;
+  }
 
   // checks for uppercase
   if (name && name == name.toUpperCase()) return `HELLO ${name}!`;
